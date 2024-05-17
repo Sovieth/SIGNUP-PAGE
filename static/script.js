a42188f77c64046595e149f9a2e9476d54596069
Sovieth <sovietthembalethu636@gmail.com>
2024-05-17 08:12:21 +0200

    admin signup, login and add bookibgs

--- static/script.js
+++ static/script.js
@@ -4,7 +4,6 @@
  window.location.href = "http://127.0.0.1:5000/signup";
  }
 
-
  function goToLandingPag2( ) {
   // Replace the URL "/landing-page" with the actual URL or route of the landing page
  window.location.href = "http://127.0.0.1:5000/login";
